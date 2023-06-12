68ab7f10326e575a1de14f1b5fa7bc8edf256d71
unknown <mendoza.luana99@gmail.com>
2023-06-12 18:37:05 -0500

    Reto-06

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import header from "./modules/header.js";
+import mode from "./modules/mode.js";
+import slider from "./modules/slider.js";
+import faq from "./modules/faq.js";
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    header();
+    mode();
+    slider();
+    faq();
+
+});
